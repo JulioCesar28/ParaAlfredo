b4e0ab396a340f758e1abf25146804d8a1b0d195
HectorBlisS <contacto@fixter.org>
2016-09-03 10:58:54 -0500

    materialized

--- cars/urls.py
+++ cars/urls.py
@@ -2,5 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.Home.as_view(), name="home"),
+	url(r'^$', views.Home.as_view(), name="list"),
+	url(r'^(?P<pk>\d+)/$', views.CarDetail.as_view(),name="detail"),
+	url(r'^nuevo/$', views.CarCreation.as_view(), name="new"),
+	url(r'^editar/(?P<pk>\d+)/$', views.CarUpdate.as_view(), name="update"),
+	url(r'^borrar/(?P<pk>\d+)/$', views.CarDelete.as_view(), name="delete"),
+
 ]
